72cd1f194e8f9436f654cc25c06ae905611200a5
Greyeminences <120108168+Greyeminences@users.noreply.github.com>
2023-05-28 23:10:15 +0200

    Add files via upload

---
+++ Pricetag.py
@@ -0,0 +1,26 @@
+price = 1000
+
+try:
+    price = int(input("How much you love me in range 0 - 100: "))
+except ValueError:
+    print("Robert, you're dumb!")
+
+# print(value)
+
+# [0, 1, 2, ..., 10]
+# elif price in list(range(10+1, 50+1))
+
+if price <= 0:
+    print(price, "that's so sad")
+elif price > 0 and price <= 10:
+    print(price, "it's a lot")
+elif price > 10 and price <= 50:
+    print(price, "it's even more")
+elif price > 50 and price <= 99:
+    print(price, "oh man")
+else:
+    print("woah, me too")
+
+# :)
+# Robert was there (:
+# <3
